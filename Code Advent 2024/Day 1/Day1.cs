a2c3daad84a3db639eb903357ac2441bd9a7ebe4
WarriorAlexOne <warrioralexone@gmail.com>
2024-12-07 01:14:52 -0600

    Day 1 part 2 completed

--- Code Advent 2024/Day 1/Day1.cs
+++ Code Advent 2024/Day 1/Day1.cs
@@ -24,27 +24,47 @@ class Day1 {
         }
 
 
-        Array.Sort(list1);   // Sort list1 from lowest to highest number.
-        Array.Sort(list2);   // Same for list2.
+        int[] sortedList1 = list1;   // New sorted lists to preserve old lists.
+        int[] sortedList2 = list2;
+        Array.Sort(sortedList1);   // Sort lists from lowest to highest number.
+        Array.Sort(sortedList2);
 
         int[] listDifferences = new int[listLength];   // List of the differences between list1 and list2.
+        int listResult = 0;   // Used to store result of adding every value in the differences list.
         for (int i = 0; i < listLength; i++) {
-            listDifferences[i] = list1[i] - list2[i];
+            listDifferences[i] = sortedList1[i] - sortedList2[i];
             if (listDifferences[i] < 0) {
                 listDifferences[i] *= -1;
             }
-        }
-
-
-        int listResult = 0;   // Used to store result of adding every value in the differences list.
-        for (int i = 0; i < listLength; i++) {
             listResult += listDifferences[i];
         }
+
         Console.WriteLine("Result: " + listResult);
 
 
         // for (int i = 0; i < listLength; i++) {
         //     Console.WriteLine(list1[i] + " List 1\n" + list2[i] + " List 2");
         // }
+
+
+        /*************************************************************************************************/
+        /*                                     Advent of Code PART 2                                     */
+        /*************************************************************************************************/
+
+
+        int[] similarityScores = new int[listLength];
+        int similarityResult = 0;
+        for (int i = 0; i < listLength; i++) {
+            int numAmount = 0;   // Used to keep track of how many times a number in the first list repeats in the second list.
+            for (int j = 0; j < listLength; j++) {
+                if (list1[i] == list2[j]) {
+                    numAmount++;   // Add 1 for everytime a repeated number is found in list2.
+                }
+            }
+            similarityScores[i] = list1[i]*numAmount;   // Multiply the number with the amount of times it repeats to get the Similarity Score.
+            similarityResult += similarityScores[i];
+        }
+
+        Console.WriteLine("Similarity Score: " + similarityResult);
     }
 }
